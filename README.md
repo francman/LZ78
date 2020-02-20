e23945a1cf93c67b9a1f99077c2da06b0edc9bc0
francman <frankmanu500@gmail.com>
2020-02-20 01:10:20 -0500

    jobs started

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # LZ78
+
+### Setup
+
+- [X] Install Python and add to PATH
+- [X] Clone or Download this directory
+- [X] Run the following command
+* __pip install -r requirements.txt__
+
+### How To Run
+
+### Copyright Information
---
+++ decompress.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2020 Frank Manu
+#
+# This source code is licensed under MIT license fount in the
+# LICENSE file in the root directory of this source tree
+#
+# The script is created in fulfillment of required coursework
+# from the Coding and Information Theory class at UMass Lowell
+# EECE 5480 - Spring 2020
+
+def decompress():
+
+if __name__ == '__main__':
+    decompress()
